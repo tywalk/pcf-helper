8a66926f236070afab62aa6a2cff4d31f33b29e3
tywalk <ty.j.walker686@gmail.com>
2026-03-26 17:51:09 -0600

    fix(pcf-helper-run): added semantic-release to streamline releases

---
+++ packages/pcf-helper-run/release.config.js
@@ -0,0 +1,23 @@
+export default {
+  branches: ['master'],
+  plugins: [
+    ['@semantic-release/commit-analyzer', {
+      releaseRules: [
+        { scope: 'pcf-helper-run', type: 'feat',  release: 'minor' },
+        { scope: 'pcf-helper-run', type: 'fix',   release: 'patch' },
+        { scope: 'pcf-helper-run', type: 'perf',  release: 'patch' },
+        // commits without this scope won't trigger a release
+        { scope: '!pcf-helper-run', release: false },
+      ],
+    }],
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/npm', {
+      npmPublish: true,
+      pkgRoot: '.',
+    }],
+    '@semantic-release/git',
+    ['@semantic-release/github', {
+      releaseNameTemplate: 'pcf-helper-run@v<%= nextRelease.version %>',
+    }],
+  ],
+};
